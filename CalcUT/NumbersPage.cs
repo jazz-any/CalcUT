8bbb1b94b5052438dfa10a41198b95189321395a
Anastasiia Vasenko <anastasiia.vasenko@synapse.com>
2015-11-21 13:33:00 +0200

    initital commit

---
+++ CalcUT/NumbersPage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace CalcUT
+{
+    public class NumbersPage
+    {
+        private Window window;
+        private Dictionary<string, Button> symbolToButtonMappings;
+
+        public NumbersPage(Window window)
+        {
+            this.window = window;
+
+            var symbolToButtonNameMappings = Enumerable.Range(0, 10)
+                .Select(i => i.ToString()).Concat(new [] {"." })
+                .ToDictionary(i => i, i => i);
+            symbolToButtonNameMappings["-"] = "+/-";
+
+            symbolToButtonMappings = symbolToButtonNameMappings
+                .ToDictionary(mapping => mapping.Key,
+                    mapping => window.Get<Button>(SearchCriteria.ByText(mapping.Value)));
+
+            //var searchCriterias = buttonNames.Select(name => SearchCriteria.ByText(name));
+            //buttons = searchCriterias.Select(criteria => window.Get<Button>(criteria)).ToArray();
+        }
+        // window.Get<Button>(SearchCriteria.ByText("1"))
+
+        public void Parse(decimal number)
+        {
+            foreach (var c in number.ToString())
+            {
+                symbolToButtonMappings[c.ToString()].Click();
+            }
+        }
+
+        //public void Parse(decimal number)
+        //{
+        //    foreach (var c in number.ToString())
+        //    {
+        //        var searchCriteria = SearchCriteria.ByText(c.ToString());
+        //        var button = window.Get<Button>(searchCriteria);
+        //        button.Click();
+        //    }
+        //}
+    }
+}
---
+++ CalcUT/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.WindowItems;
+
+namespace CalcUT
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void A()
+        {
+            var applicationDirectory = TestContext.CurrentContext.TestDirectory;
+            var applicationPath = Path.Combine(@"D:\users\anastasiia.vasenko\Desktop\PracticalTask",
+                "AvalonCalculator.exe");
+            Application application = Application.Launch(applicationPath);
+            using (Window window = application.GetWindow("WPF Calculator", InitializeOption.NoCache))
+            {
+                //window.Get<Button>(SearchCriteria.ByText("1")).Click();
+                var page = new NumbersPage(window);
+                page.Press(3);
+            }
+        }
+    }
+}
