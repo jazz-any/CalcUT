ad021b1c4d34d5e3b6264dab922ebc051cca18fe
jazz-any <nastia.vasenko@gmail.com>
2015-11-21 14:05:52 +0200

    added uncommited file

---
+++ CalcUT/OperationsPage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace CalcUT
+{
+    public class OperationsPage
+    {
+        private readonly Window window;
+        private readonly Dictionary<string, Button> operationButtons;
+
+        public OperationsPage(Window window)
+        {
+            this.window = window;
+
+            string[] operations = { "*", "/", "-", "+", "=" };
+
+            operationButtons = operations.ToDictionary(o => o, o => window.Get<Button>(SearchCriteria.ByText(o)));
+        }
+
+        public void SendNumber(decimal number)
+        {
+            foreach (var c in number.ToString())
+            {
+                operationButtons[c.ToString()].Click();
+            }
+        }
+    }
+}
