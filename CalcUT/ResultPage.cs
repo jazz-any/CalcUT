e9253683b07a50b33e985225670046dce8e469fa
jazz-any <nastia.vasenko@gmail.com>
2015-11-24 20:07:43 +0200

    tests are created

---
+++ CalcUT/ResultPage.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+using System.Windows.Automation;
+
+namespace CalcUT
+{
+    public class ResultPage
+    {
+        private readonly Window window;
+        public Button resultButton;
+        public TextBox resultField;
+        public TextBox resultScreen;
+        public SearchCriteria errorMessage;
+        public SearchCriteria okButton;
+
+        public ResultPage(Window window)
+        {
+            this.window = window;
+
+            var edits = window.GetMultiple(SearchCriteria.ByControlType(ControlType.Edit)).Cast<TextBox>().ToArray();
+            resultField = edits.First(edit => edit.Text == "0");
+            resultScreen = edits.First(edit => edit != resultField);
+            resultButton = window.Get<Button>(SearchCriteria.ByText("="));
+            errorMessage = SearchCriteria.ByText("Operation cannot be perfomed");
+            okButton = SearchCriteria.ByText("OK");
+        }
+
+        public Result GetResult()
+        {
+            resultButton.Click();
+            var modalWindow = window.ModalWindows().FirstOrDefault();
+            if (modalWindow != null)
+            {
+                var errorMessage = modalWindow.Get<Label>().Text;
+                modalWindow.Get<Button>(okButton).Click();
+                return new Result {IsSuccessful = false, ErrorMessage = errorMessage};
+            }
+            var result = resultField.Text;
+
+            return new Result {IsSuccessful = true, Value = result};
+        }
+    }
+
+    public class Result
+    {
+        public bool IsSuccessful { get; set; }
+        public string Value { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
--- CalcUT/Test.cs
+++ CalcUT/Test.cs
@@ -2,10 +2,13 @@
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Windows.Automation;
 using System.Xml.Linq;
 using NUnit.Framework;
 using TestStack.White;
 using TestStack.White.Factory;
+using TestStack.White.UIItems;  
+using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.WindowItems;
 
 namespace CalcUT
@@ -16,7 +19,9 @@ namespace CalcUT
 
         private NumbersPage numbersPage;
         private OperationsPage operationsPage;
+        private ResultPage resultPage;
         private Window window;
+
 
         [SetUp]
         public void SetUp()
@@ -26,24 +31,18 @@ namespace CalcUT
                 "AvalonCalculator.exe");
             Application application = Application.Launch(applicationPath);
             window = application.GetWindow("WPF Calculator", InitializeOption.NoCache);
+
             //window.Get<Button>(SearchCriteria.ByText("1")).Click();
             numbersPage = new NumbersPage(window);
             operationsPage = new OperationsPage(window);
-            
+            resultPage = new ResultPage(window);
+
         }
 
         [TearDown]
         public void CloseWindow()
         {
             window.Dispose();
-        }
-
-        [Test]
-        public void A()
-        {
-            decimal a = 3;
-            decimal b = 0;
-            var c = a/b;
         }
 
         public static IEnumerable<XElement> DivisionTestCases = ExternalDataHelper.Parser();
@@ -54,20 +53,35 @@ namespace CalcUT
         {
             var testCaseInput = testDataElement.Element("Input").Value;
             var expressions = Expression.Parse(testCaseInput);
+            var testCaseResult = testDataElement.Element("Result")?.Value;
+            var testCaseError = testDataElement.Element("Error")?.Value;
+
             foreach (var expression in expressions)
             {
-                if (expression is Number)
+                if (expression is Expression.Number)
                 {
-                    var numberExpression = (Number) expression;
+                    var numberExpression = (Expression.Number) expression;
                     numbersPage.SendNumber(numberExpression.Value);
                 }
-                else if (expression is Operation)
+                else if (expression is Expression.Operation)
                 {
-                    var operation = (Operation) expression;
+                    var operation = (Expression.Operation) expression;
                     operationsPage.SendOperation(operation.Operator);
                 }
             }
 
+            var result = resultPage.GetResult();
+            if (testCaseResult != null)
+            {
+                Assert.That(result.IsSuccessful, Is.True);
+                Assert.That(result.Value, Is.EqualTo(testCaseResult));
+            }
+
+            if (testCaseError != null)
+            {
+                Assert.That(result.IsSuccessful, Is.False);
+                Assert.That(result.ErrorMessage, Is.EqualTo(testCaseError));
+            }
         }
     }
 
@@ -77,79 +91,85 @@ namespace CalcUT
         {
             const char divisionOperator = '/';
             var numbers = expressionString.Split(divisionOperator).Select(number => decimal.Parse(number)).ToList();
-            var expressions = new List<Expression> { new Number { Value = numbers.First() } };
+            var expressions = new List<Expression> {new Number {Value = numbers.First()}};
             foreach (var number in numbers.Skip(1))
             {
                 expressions.Add(new DivisionOperation());
-                expressions.Add(new Number { Value = number });
+                expressions.Add(new Number {Value = number});
             }
 
             return expressions;
-        } 
+        }
+
+
+
+
+        public abstract class Operation : Expression
+        {
+            public abstract string Operator { get; }
+        }
+
+        public class DivisionOperation : Operation
+        {
+            public override string Operator
+            {
+                get { return "/"; }
+            }
+        }
+
+        public class Number : Expression
+        {
+            public decimal Value { get; set; }
+        }
+
+        //public static class ExpressionParser
+        //{
+
+
+        //    public static Expression Parse(string expressionString)
+        //    {
+
+        //    }
+        //}
+
+        //public abstract class Expression
+        //{
+        //    public abstract decimal Value { get; }        
+        //}
+
+        //public class Number : Expression
+        //{
+        //    public static Regex NumberRegex = new Regex("(\\d+)");
+
+        //    public Number(decimal value)
+        //    {
+        //        Value = value;
+        //    }
+
+        //    public override decimal Value { get; }
+        //}
+
+        //public abstract class BinaryOperation : Expression
+        //{
+        //    public abstract Expression Left { get; }
+        //    public abstract Expression Right { get; }
+        //}
+
+        //public class DivisionOperation : BinaryOperation
+        //{
+        //    public DivisionOperation(Expression left, Expression right)
+        //    {
+        //        Left = left;
+        //        Right = right;
+        //    }
+
+        //    public override decimal Value
+        //    {
+        //        get { return Left.Value/Right.Value; }
+        //    }
+
+        //    public override Expression Left { get; }
+        //    public override Expression Right { get; }
+        //}
     }
-
-    public abstract class Operation : Expression
-    {
-        public abstract string Operator { get; }
-    }
-
-    public class DivisionOperation : Operation
-    {
-        public override string Operator { get { return "/"; } }
-    }
-
-    public class Number : Expression
-    {
-        public decimal Value { get; set; }
-    }
-
-    //public static class ExpressionParser
-    //{
-        
-
-    //    public static Expression Parse(string expressionString)
-    //    {
-            
-    //    }
-    //}
-
-    //public abstract class Expression
-    //{
-    //    public abstract decimal Value { get; }        
-    //}
-
-    //public class Number : Expression
-    //{
-    //    public static Regex NumberRegex = new Regex("(\\d+)");
-
-    //    public Number(decimal value)
-    //    {
-    //        Value = value;
-    //    }
-
-    //    public override decimal Value { get; }
-    //}
-
-    //public abstract class BinaryOperation : Expression
-    //{
-    //    public abstract Expression Left { get; }
-    //    public abstract Expression Right { get; }
-    //}
-
-    //public class DivisionOperation : BinaryOperation
-    //{
-    //    public DivisionOperation(Expression left, Expression right)
-    //    {
-    //        Left = left;
-    //        Right = right;
-    //    }
-
-    //    public override decimal Value
-    //    {
-    //        get { return Left.Value/Right.Value; }
-    //    }
-
-    //    public override Expression Left { get; }
-    //    public override Expression Right { get; }
-    //}
 }
